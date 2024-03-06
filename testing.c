90e36883c589e09e0571b47dff52248ba4368006
Fabian Rapp <frapp@1-E-24.42heilbronn.de>
2024-03-06 09:53:10 +0100

    a

--- testing.c
+++ testing.c
@@ -6,7 +6,7 @@
 /*   By: frapp <frapp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 15:40:37 by frapp             #+#    #+#             */
-/*   Updated: 2024/03/06 03:26:22 by frapp            ###   ########.fr       */
+/*   Updated: 2024/03/06 09:53:04 by frapp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,3 @@ int main(int ac, char *av[])
 	printf("sizeof t_redir: %lu\n", sizeof(t_redir));
 	return 0;
 }
-
-// void print_parser_testing(t_parser *head)
-// {
-// 	t_parser *current;
-// 	static int	count = 0;
-// 	int	inter_count = 100;
-
-// 	current = head;
-// 	while (current != NULL && current->p_type != T_EOF)
-// 	{
-// 		if (++count > inter_count)
-// 		{
-// 			printf("inf loop (reached %d iterations)\n", inter_count);
-// 			exit (1);
-// 		}
-// 		char	*str_data = current->token->str_data;
-// 		printf("%s: %s\n", type_to_str_type(current->p_type), str_data);
-// 		if (current->arg)
-// 		{
-// 			printf("args:\n");
-// 			print_parser_testing(current->arg);
-// 		}
-// 		current = current->next;
-		
-// 		printf("--------------------------------------\n");
-// 	}
-// }
