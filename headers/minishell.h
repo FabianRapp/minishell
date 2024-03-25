7daddf83ac0aa4811d4d47366a23d4a4664087a3
Partttyyy <fa.rapp@web.de>
2024-03-25 13:17:57 +0100

    idk last commit

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: frapp <frapp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 06:20:46 by frapp             #+#    #+#             */
-/*   Updated: 2024/03/25 11:01:03 by frapp            ###   ########.fr       */
+/*   Updated: 2024/03/25 13:12:54 by frapp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@
 # include "parser.h"
 
 # ifndef TESTER
-#  define TESTER 0
+#  define TESTER 1
 # endif
 
 # ifndef SHELL_NAME
